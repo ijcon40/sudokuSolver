9b1e6f58c599136c5111ae3397012c972a112ce0
Ian Conrad <ijcon40@gmail.com>
2019-04-24 11:16:44 -0400

    hahah hee hoo

---
+++ src/CNN/Network.java
@@ -0,0 +1,46 @@
+package CNN;
+
+public class Network{
+    Neuron[][] Neurons;
+
+    public Network(int depth, int width){
+        this.Neurons = new Neuron[depth][width];//Array[x][y]
+        for(int x = 0; x<depth; x++){
+            for(int y = 0; y<width; y++){
+                Neurons[x][y] = new Neuron(width);
+            }
+        }
+    }
+
+    //Part 1: Forward Propagation
+    public double[] forwardProp(double[] input, int layer){
+        //Resolve: if there is no layer or out of bounds, then return the input
+        if(layer>Neurons.length){
+            return input;
+        }
+        //Calculate the weight layer that needs to be passed to every neuron
+        double[] ReLuOutputArray = new double[Neurons[0].length];
+        int index = 0;
+        for(Neuron neuron: Neurons[layer]){
+            ReLuOutputArray[index] = neuron.getActivatedOutput(input);
+            index++;
+        }
+        return forwardProp(ReLuOutputArray, layer+1);
+    }
+
+    public double[] getLoss(double[] expected, double[] actual){
+        double[] PointwiseLoss = new double[expected.length];
+        for(int counter = 0; counter<expected.length; counter++){
+            PointwiseLoss[counter] = actual[counter]-expected[counter];
+        }
+        return PointwiseLoss;
+    }
+
+    public double[] backProp(double[] passedDerivatives, int layer){
+        //Resolve: if layer==-1 then return the passed derivatives for convolution back prop
+    }
+
+
+    //Part 2: Back Propagation
+
+}
--- src/CNN/Neuron.java
+++ src/CNN/Neuron.java
@@ -3,6 +3,7 @@ package CNN;
 public class Neuron {
     private double weights[];
     private double bias;
+    private double ReLuSlope;
     private Equation ReLu = (double... x)->{
         //A Leaky ReLU function abbreviated for simpler notation
         if(x[0]>0){
@@ -32,6 +33,7 @@ public class Neuron {
     }
 
     private void HeEtAlWeights(){
+        //This may not overwrite the
         for(double weight: weights){
             weight = Math.random()*Math.sqrt(2/weights.length);
         }
@@ -47,9 +49,22 @@ public class Neuron {
     }
 
     public double getActivatedOutput(double[] inputs){
-        return ReLu.resolve(getRawOutput(inputs));
+        //Adding the saving of the ReLu slope with respect to the outputs should make the backprop much easier
+        double val = ReLu.resolve(getRawOutput(inputs));
+        if(val<0){
+            ReLuSlope = .01;
+        }
+        else{
+            ReLuSlope = 1.0;
+        }
+        return val;
     }
 
+    public double[] getDerivatives(double[] prevDerivs){
+        //So dL/dO will be passed, but the first layer will be all 1 since they are additive
+        //Next we just need to see how the previous layer outputs affected this one neuron. Bias can be ignored since derivative of bias is just 0
+        //First Step: Take the derivative of all the prevDerivs with respect to ReLu
+    }
 
 
 }
